6235845577607f460775e43df7cacf8f15342c39
egsam98 <egsam98@gmail.com>
2020-04-14 01:16:40 +0300

    actionTypes for AuthorsList

--- pages/containers/AuthorsList/actions.js
+++ pages/containers/AuthorsList/actions.js
@@ -1,18 +1,19 @@
 import {put, takeEvery} from "redux-saga/effects";
 import {fetchAuthors} from "../../../api/authors_api";
+import {GET_AUTHORS, GET_AUTHORS_FAILURE, GET_AUTHORS_SUCCESS} from "./actionTypes";
 
 
-export const getAuthors = () => ({type: 'GET_AUTHORS'})
+export const getAuthors = () => ({type: GET_AUTHORS})
 
 function* getAuthorsGen() {
     try {
         const authors = yield fetchAuthors()
-        yield put({type: "GET_AUTHORS_SUCCESS", payload: authors});
+        yield put({type: GET_AUTHORS_SUCCESS, payload: authors});
     } catch (err) {
-        yield put({type: 'GET_AUTHORS_FAILURE', payload: `Internal error: ${err.message}`})
+        yield put({type: GET_AUTHORS_FAILURE, payload: `Internal error: ${err.message}`})
     }
 }
 
 export function* authorsWatcher() {
-    yield takeEvery('GET_AUTHORS', getAuthorsGen)
+    yield takeEvery(GET_AUTHORS, getAuthorsGen)
 }
--- pages/containers/AuthorsList/reducers.js
+++ pages/containers/AuthorsList/reducers.js
@@ -1,3 +1,6 @@
+import {GET_AUTHORS, GET_AUTHORS_SUCCESS, GET_AUTHORS_FAILURE} from "./actionTypes";
+
+
 const initialState = {
     isLoading: false,
     authors: [],
@@ -7,11 +10,11 @@ const initialState = {
 export default (state=initialState, action) => {
     state.error = null
     switch (action.type) {
-        case 'GET_AUTHORS':
+        case GET_AUTHORS:
             return {...state, isLoading: true}
-        case 'GET_AUTHORS_SUCCESS':
+        case GET_AUTHORS_SUCCESS:
             return {...state, authors: action.payload, isLoading: false}
-        case 'GET_AUTHORS_FAILURE':
+        case GET_AUTHORS_FAILURE:
             return {...initialState, error: action.payload}
         default:
             return state
